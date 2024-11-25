114249db9c115abd9f37fe0d7b55c4917a416354
Leandrasenac <lelesilvaramm@gmail.com>
2024-11-25 14:57:33 +0000

    Gravar arquivos

---
+++ phyton/aula2.py
@@ -0,0 +1,10 @@
+faturamento = 1200 # Tipo: int -> número inteiro.
+custo = 750.32 # Tipo: float -> número com casas decimais
+lucro = faturamento - custo
+
+margem_lucro = lucro / faturamento
+
+print("faturamento foi de ",faturamento)
+print("O custo foi de", custo)
+print("O lucro foi de ",lucro)
+print("A margem_lucro foi de ", margem_lucro)
