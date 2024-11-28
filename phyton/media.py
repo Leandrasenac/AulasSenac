11fd6efe2a190f6e69094afafad968c046640f8a
Leandrasenac <lelesilvaramm@gmail.com>
2024-11-28 11:31:42 +0000

    Gravar arquivo

--- phyton/media.py
+++ phyton/media.py
@@ -1,7 +1,7 @@
 valor1 = valor2 = M = float
-valor1 = float(input ("Digite o primeiro valor: "))
 
-valor2= float(input("Digite o primeiro valor: "))
+valor1 = float(input ("Digite o primeiro valor: "))
+valor2= float(input("Digite o segundo valor: "))
 
 M = (valor1 + valor2) / 2
 
