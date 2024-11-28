5126b3321e2445c49b62ece14a0e11c736cd2e46
Leandrasenac <lelesilvaramm@gmail.com>
2024-11-28 11:21:53 +0000

    Gravar arquivo

---
+++ phyton/media.py
@@ -0,0 +1,10 @@
+valor1 = valor2 = M = float
+valor1 = float(input ("Digite o primeiro valor: "))
+
+valor2= float(input("Digite o primeiro valor: "))
+
+M = (valor1 + valor2) / 2
+
+
+print("A média entre ",valor1," e ",valor2, " é ", M)
+print(M)
